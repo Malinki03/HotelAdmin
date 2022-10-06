59614edddbffeeafde2c9f638f803f70fbef8a2a
nesgon <prova@DESKTOP-IJ2PFF8>
2022-10-06 21:16:43 +0200

    borrado

--- M13TipusHab/Controller/Program.cs
+++ M13TipusHab/Controller/Program.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-//Mostopapi cuando una actriz porno le dice que le come la polla los huevos la polla mmmmmm
 
 namespace M13TipusHab.Controller
 {
